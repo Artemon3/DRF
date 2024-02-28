bc49f67bc6b30238694b51a89291baca663d2b90
Artem <levkin_mizuno@mail.ru>
2024-02-28 15:59:00 +0700

    ``

--- course/permissions.py
+++ course/permissions.py
@@ -6,3 +6,11 @@ class IsOwner(permissions.BasePermission):
         if request.method in permissions.SAFE_METHODS:
             return True
         return obj.owner == request.user
+
+
+class IsModerator(permissions.BasePermission):
+
+    def has_permission(self, request, view):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return request.user.groups.filter(name='Модераторы').exists()
--- users/views.py
+++ users/views.py
@@ -1,4 +1,4 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions
 from rest_framework.filters import OrderingFilter
 from django_filters.rest_framework import DjangoFilterBackend
 
@@ -9,9 +9,11 @@ from users.serializers import PaySerializer, UserSerializer
 class PayViewSet(viewsets.ModelViewSet):
     serializer_class = PaySerializer
     queryset = Pay.objects.all()
+    permission_classes = [permissions.IsAdminUser]
     filter_backends = [DjangoFilterBackend, OrderingFilter]
     filterset_fields = ['lesson', 'course', 'payment_method']
     ordering_fields = ['date']
+
 
 class UserViewSet(viewsets.ModelViewSet):
     serializer_class = UserSerializer
